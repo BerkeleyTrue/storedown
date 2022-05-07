ce6972e756fd5a7d1e3f0ff946825049487b2c76
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-05-07 00:06:52 -0700

    style(comp/List): Prettier

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,31 +1,29 @@
 <template>
-<v-container grid-list-lg fluid>
-  <v-layout>
-    <v-flex xs6>
-      <h4 class="display-1">Item list</h4>
-    </v-flex>
-  </v-layout>
-  <!-- TODO QR code link generator -->
-  <List :headers="headers" :loadData="loadData" :rowClick="rowClick" />
-</v-container>
+  <v-container grid-list-lg fluid>
+    <v-layout>
+      <v-flex xs6>
+        <h4 class="display-1">Item list</h4>
+      </v-flex>
+    </v-layout>
+    <!-- TODO QR code link generator -->
+    <List :headers="headers" :loadData="loadData" :rowClick="rowClick" />
+  </v-container>
 </template>
 
 <script>
-import {
-  ConfigService
-} from '../services/Config.js'
-import DatabaseService from '../services/Database.js'
-import List from '../components/List'
+import { ConfigService } from '../services/Config.js';
+import DatabaseService from '../services/Database.js';
+import List from '../components/List';
 
 export default {
   components: {
-    List
+    List,
   },
   data: () => ({
-    headers: []
+    headers: [],
   }),
-  mounted () {
-    this.loadHeader()// Don't need to reload with route change
+  mounted() {
+    this.loadHeader(); // Don't need to reload with route change
   },
   methods: {
     /**
@@ -33,29 +31,34 @@ export default {
      */
     loadHeader: function () {
       if (!DatabaseService()) {
-        return
+        return;
       }
 
       // Get config and headers
-      ConfigService.getConfigObject().then((config) => {
-        config.dataDefinition.forEach((group) => {
-          group.fields.forEach((item) => {
-            if (item.hideInList) { return }
+      ConfigService.getConfigObject()
+        .then((config) => {
+          config.dataDefinition.forEach((group) => {
+            group.fields.forEach((item) => {
+              if (item.hideInList) {
+                return;
+              }
 
-            this.headers.push({
-              text: item.displayName,
-              value: item.name,
-              align: 'left',
-              item: item, // So you can access all the parameters. Above are given in the data table support format.
-              sortable: false
-            })
-          })
+              this.headers.push({
+                text: item.displayName,
+                value: item.name,
+                align: 'left',
+                item: item, // So you can access all the parameters. Above are given in the data table support format.
+                sortable: false,
+              });
+            });
+          });
         })
-      }).catch((error) => {
-        if (error.status === 404) {} else {
-          this.$toasted.info(`Error ${error}`)
-        }
-      })
+        .catch((error) => {
+          if (error.status === 404) {
+          } else {
+            this.$toasted.info(`Error ${error}`);
+          }
+        });
     },
     /**
      * Load data
@@ -66,22 +69,24 @@ export default {
     loadData: function (skip = 0, limit = 100) {
       return new Promise((resolve, reject) => {
         if (!DatabaseService()) {
-          return reject(new Error('Database is not setup'))
+          return reject(new Error('Database is not setup'));
         }
 
         // Get items
-        DatabaseService().getAllItems({ skip, limit }).then((data) => {
-          return resolve(data) // important we save an original copy. So we dont need to load it again. Espicially in tree view.
-        })
-      })
+        DatabaseService()
+          .getAllItems({ skip, limit })
+          .then((data) => {
+            return resolve(data); // important we save an original copy. So we dont need to load it again. Espicially in tree view.
+          });
+      });
     },
     /**
      * Hande logic for when a row is clicked
      * @param  {[type]} props  [description]
      */
     rowClick: function (props) {
-      return this.$router.push(`/entry/${props.item._id}`)
-    }
-  }
-}
+      return this.$router.push(`/entry/${props.item._id}`);
+    },
+  },
+};
 </script>
